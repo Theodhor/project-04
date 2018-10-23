007a2c9b0eab41fe652265c1c78948e72c4b9190
Theodhor <s.theodhor@yahoo.it>
2018-10-23 16:35:40 +0100

    Initial commit

---
+++ config/routes.py
@@ -0,0 +1,10 @@
+import os
+from app import app
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<pat:path>')
+def catch_all(path):
+    if os.path.isfile('public/' + path):
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html')    
