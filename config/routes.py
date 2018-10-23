df3659aa93c5d0f62f1c7ff10d289d82ed07ff50
Theodhor <s.theodhor@yahoo.it>
2018-10-23 17:03:52 +0100

    typo important

--- config/routes.py
+++ config/routes.py
@@ -2,9 +2,9 @@ import os
 from app import app
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<pat:path>')
+@app.route('/<path:path>')
 def catch_all(path):
     if os.path.isfile('public/' + path):
         return app.send_static_file(path)
 
-    return app.send_static_file('index.html')    
+    return app.send_static_file('index.html')
