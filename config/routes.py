38b9427c508999dbd55c70b5c068cfc2d5dffa96
Theodhor <s.theodhor@yahoo.it>
2018-11-02 10:14:09 +0000

    I must listen to my instructor

--- config/routes.py
+++ config/routes.py
@@ -14,7 +14,7 @@ app.register_blueprint(list.api, url_prefix='/api')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    if os.path.isfile('src/' + path):
+    if os.path.isfile('public/' + path):
         return app.send_static_file(path)
 
     return app.send_static_file('index.html')
